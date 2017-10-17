0f605f41c43700690c4065a235895982ce40cb1e
TheNecker <chrisp132012@gmail.com>
2017-10-17 13:24:40 -0400

    working

--- app.rb
+++ app.rb
@@ -13,17 +13,17 @@ enable :sessions
 	post '/select_players' do
 		session[:player1_type] = params[:player1]
 		session[:player2_type] = params[:player2]
+		puts "#{session[:player2_type]} play 2 type"
 		session[:human1] = 'no'
 		session[:human2] = 'no'
 		if session[:player1_type] == 'Human'
 			session[:player1] = Human.new('X')
 			session[:human1] = 'yes'
-
 		elsif session[:player1_type] == 'Easy'
 			session[:player1] = Sequential.new('X')
 		elsif session[:player1_type] == 'Medium'
 			session[:player1] = RandomAI.new('X')
-		elsif session[:player1_type] == 'Impossible!'
+		elsif session[:player1_type] == 'Unbeatable'
 			session[:player1] = UnbeatableAI.new('X')
 		end
 
@@ -34,35 +34,33 @@ enable :sessions
 			session[:player2] = Sequential.new('O')
 		elsif session[:player2_type] == 'Medium'
 			session[:player2] = RandomAI.new('O')
-		elsif session[:player2_type] == 'Impossible!'
+		elsif session[:player2_type] == 'Unbeatable'
 			session[:player2] = UnbeatableAI.new('O')
 		# else
 		# 	redirect '/'
 		end
-
+puts "#{session[:player2]} playe 2"
 		session[:active_player] = session[:player1]
+		puts "#{session[:active_player]} ples work"
 		if session[:human1] == 'yes'
 			redirect '/board'
 		else
 			redirect '/make_move'
 		end
 	end
-
+	
 	get '/board' do
-
 		erb :board, :locals => {player1: session[:player1], player2: session[:player2], active_player: session[:active_player].marker, board: session[:board]}
-
 	end
 
-	get '/make_move' do
+	get '/make_move' do 
+		puts "#{session[:active_player]} here"
 		move = session[:active_player].get_move(session[:board].ttt_board)
 		session[:board].update_position(move, session[:active_player].marker)
 		redirect '/check_game_state'
-
 	end
 
 	post '/human_move' do
-
 		move = params[:choice].to_i - 1
 		if session[:board].valid_position?(move)
 			session[:board].update_position(move, session[:active_player].marker)
@@ -81,11 +79,13 @@ enable :sessions
 			message = "Cat took the game, sorry."
 			erb :game_over, :locals => {board: session[:board], message: message}
 		else
+			puts "#{session[:active_player]} before change"
 			if session[:active_player] == session[:player1]
 				session[:active_player] = session[:player2]
 			else
 				session[:active_player] = session[:player1]
 			end
+			puts "#{session[:active_player]} after change"
 			if session[:active_player] == session[:player1] && session[:human1] == 'yes' || session[:active_player] == session[:player2] && session[:human2] == 'yes'
 				redirect '/board'
 			else
